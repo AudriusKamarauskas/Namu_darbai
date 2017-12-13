e0b6443ffa5daf08e180e3aada920bd56d08b90f
AudriusKamarauskas <kamarauskas.aud@gmail.com>
2017-12-13 19:57:22 +0200

    Pirmas comitas

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Puslapio antraštė</title>
+        <meta charset="utf-8">
+<!--        Fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+<!--        Styles-->
+        <link rel="stylesheet" type="text/css" href="reset.css">
+        <link rel="stylesheet" type="text/css" href="style.css?v=3">
+    </head>
+    
+    <body>
+        <?php
+            // $a = 2;
+            // $b = $a * 3;
+
+            // $pareigos = "Marshal";
+            // $pavarde = 'D\'Artagnan';
+
+            // $pasisveikinimas = "Hello " . $pareigos . " " . $pavarde;
+
+            // echo $pasisveikinimas;
+
+            // $a = -2 +4;
+            // $a = $a *3;
+            // $c = $a / 4;
+            // $d = $a % 4;
+            // $a *= 2; // tas pats kas $a= $a * 2
+
+            // $a = 8;
+            // $a++;
+            // ++$a;
+            // $x = $a-- + 2;
+            // $y = ++$a *2;
+
+            // $y = htmlentities("<b>{$a}</b>");
+            // echo "<br>" . $y . "<br>";
+            // $z = html_entity_decode($y);
+            // echo $z;
+
+            // $favcolor = "red";
+
+            // switch ($favcolor) {
+            //     case "red":
+            //     echo "Your favorite color is red!";
+            //     break;
+            //     case "green":
+            //     echo "Your favorite color is green!";
+            //     break;
+            //     default:
+            //     echo "Your favorite color is neither red, blue, nor green!";
+            // }
+
+                $array = array (
+                    [1, 2, 4],
+                    [2, 10, 3],
+                    [0, 4, 9]
+                );
+
+                var_dump($array);
+
+        ?>
+
+    </body>
+    
+</html>
