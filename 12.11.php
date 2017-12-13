8fcd2898a159b569dd05bf8e313f2c1ca3a0f671
AudriusKamarauskas <34310077+AudriusKamarauskas@users.noreply.github.com>
2017-12-13 15:31:30 +0200

    Add files via upload

---
+++ 12.11.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Puslapio antraštė</title>
+        <meta charset="utf-8">
+<!--        Fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+<!--        Styles-->
+        <link rel="stylesheet" type="text/css" href="reset.css">
+        <link rel="stylesheet" type="text/css" href="style.css?v=3">
+    </head>
+    
+    <body>
+        <?php
+
+            $a = [
+                200,
+                35,
+                3,
+                5,
+                55,
+                6,
+                140,
+                8
+               ];
+
+            $x = 0;
+            $y = 0;
+            $z = 0;
+            $q = 0;
+
+            for($i = 0; $i < count($a); $i++) {
+                    $x = $x += $a[$i] / count($a);
+            }
+
+            echo 'Vidutine masyvo elementu reiksme yra ' . $x . '<br>';
+
+            for ($i = 0; $i <count($a); $i++) {
+                if ($a[$i] % 2 == 0) {
+                    $y = $y += $a[$i];
+                }
+            }
+            echo 'Masyvo lyginiu elementu suma yra ' . $y . '<br>';
+
+            sort($a);
+
+            for ($i = 0; $i < count($a); $i++) {
+                echo $a[$i] . ', ';
+            }
+            echo '<br>';
+
+            $b = [
+                [3, 5, 7, 9, 11], 
+                [10, 20, 30, 40, 50], 
+                [50, 100, 150, 200, 250],
+                [100, 200, 300, 400, 500],
+                [200, 400, 600, 800, 1000]
+            ];
+
+            $result = $b;
+
+            foreach($b[0] as $k => $v)
+                $result[$k] = array_sum(array_column($b, $k));
+
+                print_r($result);
+                echo '<br> Didziausio stulpelio suma yra ';
+                print_r(max($result));
+                
+                for ($i = 0; $i < count($b); $i++) {
+                    $z = $z += $b[$i][$i];
+                }
+
+                echo '<br> Pirmos istrizaines suma yra ' . $z;
+
+                for ($i = 0; $i < count($b); $i++) {
+                    $q = $q += array_reverse ($b)[$i][$i];
+                }
+                
+                echo '<br> Antros istrizaines suma yra ' . $q;
+                
+        ?>
+
+    </body>
+    
+</html>
