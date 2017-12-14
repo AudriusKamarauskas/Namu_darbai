fe58befc9c0ee7875143df3f010ce6fa6d2fe8a0
AudriusKamarauskas <kamarauskas.aud@gmail.com>
2017-12-14 19:25:58 +0200

    Mokomes funkcijas

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Puslapio antraštė</title>
+        <meta charset="utf-8">
+<!--        Fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+<!--        Styles-->
+        <link rel="stylesheet" type="text/css" href="reset.css">
+        <link rel="stylesheet" type="text/css" href="style.css?v=3">
+    </head>
+    
+    <body>
+        <?php
+
+        $x = 0;
+        $sum = 0;
+            while ($x < 100) {
+               
+               $sum += $x;
+               $x++;
+            }
+
+            echo $sum . '<br>';
+
+            function skyrius() {
+                echo '<hr>';
+                echo '<h1>SKYRIUS</h1>';
+                echo '<br>';
+            }
+
+            skyrius();
+            skyrius();
+
+            function faktorialas($a) {
+                if ($a == 1) {
+                    return $a;
+                } else {
+                    return $a * faktorialas($a - 1);
+                }
+            }
+
+            echo '3!='. faktorialas(3). '<br>';
+            echo '10!='. faktorialas(10). '<br>';
+
+            function hello($vardas, $pavarde) {
+                echo 'Labas! ' . $vardas . ' ' . $pavarde;
+            }
+            hello('Audrius', 'Kamarauskas');
+        ?>
+
+    </body>
+    
+</html>
