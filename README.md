a24793bb24f237645dc83c45e7ab1708244e984f
AudriusKamarauskas <kamarauskas.aud@gmail.com>
2017-12-17 17:30:58 +0200

    Funkciju namu darbas

--- README.md
+++
@@ -1,1 +0,0 @@
-# Namu_darbai
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Puslapio antraštė</title>
+        <meta charset="utf-8">
+    </head>
+    
+    <body>
+        <?php
+            function arrayAverage($array) 
+            {
+                $sum = 0;
+                for ($i = 0; $i < count($array); $i++) {
+                    $sum += $array[$i];
+                }
+
+                $average = $sum / count($array);
+
+                return $average;
+            }
+
+            $a = [5, 6, 10, 15];
+            $b = [8, 5, 3, 25];
+
+            echo arrayAverage($a) - arrayAverage($b) . '<br>';
+            
+            function isPerfectNumber($n)
+            {
+                $i = 1;
+                $sum = 0;
+                  
+                while ($i < $n)
+                {
+                    if ($n % $i == 0)
+                    {
+                        $sum = $sum + $i; 
+                    }
+                    $i++;
+                }
+                return $sum == $n;
+            }
+             
+            for ($n = 1; $n <= 1001; $n++) {
+              
+            if (isPerfectNumber($n))
+                echo $n . " Yra tobulas skaicius <br>";
+            }
+        ?>
+    </body>
+    
+</html>
