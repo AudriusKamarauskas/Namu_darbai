c7a6d2bb64be27710b90d68954778f062df9dcf3
AudriusKamarauskas <kamarauskas.aud@gmail.com>
2018-01-08 12:38:05 +0200

    Lentele

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lentelė</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    
+    <body>
+        <table border="1">
+            <tr>
+                <td colspan="2">Pavadinimas</td>
+                <td>Veiksmas</td>
+            </tr>
+            <tr>
+                <td colspan="3">Vaisiai</td>
+            </tr>
+            <tr>
+                <td>Obuolys</td>
+                <td><img src="http://s3-eu-west-1.amazonaws.com/nucig/Flavours/eliquid-apple-flavour-250.jpg"></td>
+                <td class="button">Pirkti</td>
+            </tr>
+            <tr>
+                <td>Apelsinas</td>
+                <td><img src="https://3.imimg.com/data3/YD/ME/MY-452003/fresh-orange-250x250.png"></td>
+                <td class="button">Parduoti</td>
+            </tr>
+            <tr>
+                <td colspan="3">Daržovės</td>
+            </tr>
+            <tr>
+                <td>Morka</td>
+                <td><img src="http://www.lpsrajongok.eoldal.hu/img/picture/1504/r%C3%A9pa.jpg"></td>
+                <td class="button">Pirti</td>
+            </tr>
+            <tr>
+                <td>Kopūstas</td>
+                <td><img src="https://images-na.ssl-images-amazon.com/images/I/41nZ3l1hWvL._SX355_.jpg"></td>
+                <td class="button">Parduoti</td>
+            </tr>
+            <tr>
+                <td>Bulvė</td>
+                <td><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Double_potato.jpg/320px-Double_potato.jpg"></td>
+                <td class="button">Pasiskolinti</td>
+            </tr>
+            <tr>
+                <td colspan="3">Mėsa</td>
+            </tr>
+            <tr>
+                <td>Vištiena</td>
+                <td><img src="https://worldrecipes.eu/wp-content/uploads/2017/11/kepta-vistiena-su-sviesto-ir-acto-padazu-300x196.jpg"></td>
+                <td class="button">Pirkti</td>
+            </tr>
+            <tr>
+                <td>Jautiena</td>
+                <td><img src="https://s2.15min.lt/images/photos/2013/05/09/original/jautiena-518ba3e65a536.jpg"></td>
+                <td class="button">Parduoti</td>
+            </tr>
+            <tr>
+                <td>Kiauliena</td>
+                <td><img src="https://www.gaspadine.lt/uploads/modules/recipes/ingredients/thumb250/156.jpg"></td>
+                <td class="button">Atiduoti</td>
+            </tr>
+        </table>
+    </body>
+</html>
